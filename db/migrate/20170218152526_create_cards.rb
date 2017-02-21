d8a83e75bb0be0315bbedc2aa6e9ce9fcaee4880
nishina <n.toshi.1.014@gmail.com>
2017-02-21 23:46:43 +0900

    update tables

--- db/migrate/20170218152526_create_cards.rb
+++ db/migrate/20170218152526_create_cards.rb
@@ -3,6 +3,7 @@ class CreateCards < ActiveRecord::Migration[5.0]
     create_table :cards do |t|
       t.string :name
       t.string :info
+      t.references :loan, foreign_key: true
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
-Card.create({name: 'ねこカード', info: 'ねこのカードです。'})
-Card.create({name: 'ぺるしゃねこカード', info: 'ぺるしゃねこのカードです。'})
-Card.create({name: 'いぬカード', info: 'いぬのカードです。'})
+Loan.create(amount: 100)
+Loan.create(amount: 300)
+Card.create({name: 'ねこカード', info: 'ねこのカードです。', loan_id: 1})
+Card.create({name: 'ぺるしゃねこカード', info: 'ぺるしゃねこのカードです。', loan_id: 2})
+Card.create({name: 'いぬカード', info: 'いぬのカードです。', loan_id: 1})
 Card.create({name: 'いぬねこカード', info: 'いぬねこのカードです。'})
 10.times do |i|
   Card.create(
       name:    "いぬねこカード#{i}",
-      info: "いぬねこのカードです#{i}"
+      info: "いぬねこのカードです#{i}",
+      loan_id: 1
   )
 end
