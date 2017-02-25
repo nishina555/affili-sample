f8e57499774d5d949b707ac8195d19e5b1f8a36a
nishina <n.toshi.1.014@gmail.com>
2017-02-26 01:36:51 +0900

    show seach query

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,17 +1,15 @@
 class CardsController < ApplicationController
   def index
     @q = Card.ransack(params[:q])
-    # @cards = @q.result(distinct: true)
     @cards = @q.result.includes(:loan_methods)
     @loans = Loan.all
     @loan_methods = LoanMethod.all
-
-    # @cards = Card.all
   end
 
   def search
     @q = Card.page(params[:page]).search(search_params)
     @cards = @q.result(distinct: true)
+    @search_query = params[:q]
   end
 
   private
